e581bac35a17271219224dfbd267762e3a265cfc
Alvin Yanson <a.yanson@arcanys.com>
2024-07-01 15:30:28 +0800

    feat: retrieve category then display value to form

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -46,5 +46,46 @@ namespace BulkyWeb.Controllers
             return View();
 
         }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryObj = _db.Categories.Find(id);
+            if (categoryObj == null) {
+                return NotFound();
+            }
+
+            return View(categoryObj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+
+            //if (obj.Name != null && obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "Test is an invalid value");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
     }
 }
