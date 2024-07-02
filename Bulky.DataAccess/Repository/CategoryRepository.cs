e010028914687d0342a8a8d5beb0b52371fec75a
Alvin Yanson <a.yanson@arcanys.com>
2024-07-02 13:26:04 +0800

    feat: created category repository

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+
+        private ApplicationDBContext _db;
+
+        public CategoryRepository(ApplicationDBContext db): base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
