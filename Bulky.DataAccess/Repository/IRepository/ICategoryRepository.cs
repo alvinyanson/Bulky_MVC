e319412b3dd5b704e25c7f6c6be93fcfcfd80df0
Alvin Yanson <a.yanson@arcanys.com>
2024-07-02 13:53:45 +0800

    feat: transfer Save method to UnitOfWork.cs

--- Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -5,7 +5,5 @@ namespace Bulky.DataAccess.Repository.IRepository
     public interface ICategoryRepository : IRepository<Category>
     {
         void Update(Category obj);
-
-        void Save();
     }
 }
